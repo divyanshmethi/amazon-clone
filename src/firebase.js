77de322c0c981d69039eb715cd74b720d008d05f
Divyansh.Methi <Divyansh_Methi@dell.com>
2020-09-13 19:22:43 +0530

    Firebase Changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyC4ozwpEqU24IlOzoL4h9TcnWBROUhtrRg",
-    authDomain: "dsfv-a69b3.firebaseapp.com",
-    databaseURL: "https://dsfv-a69b3.firebaseio.com",
-    projectId: "dsfv-a69b3",
-    storageBucket: "dsfv-a69b3.appspot.com",
-    messagingSenderId: "783803789887",
-    appId: "1:783803789887:web:ae8da0b071106ae8e93e8a"
-  };
+  apiKey: "AIzaSyC4ozwpEqU24IlOzoL4h9TcnWBROUhtrRg",
+  authDomain: "dsfv-a69b3.firebaseapp.com",
+  databaseURL: "https://dsfv-a69b3.firebaseio.com",
+  projectId: "dsfv-a69b3",
+  storageBucket: "dsfv-a69b3.appspot.com",
+  messagingSenderId: "783803789887",
+  appId: "1:783803789887:web:ae8da0b071106ae8e93e8a",
+};
